b9453dfdb873331508fa1ccf86fb6fb044b4bcec
MWPX777 <mwpx777@gmail.com>
2021-04-21 13:08:09 -0500

    removed part of server.js

--- server.js
+++ server.js
@@ -16,17 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useFindAndModify: false,
-//   useUnifiedTopology: true
-// });
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-    
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
