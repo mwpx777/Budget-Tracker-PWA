70d582114eb5737e1b9058fc8a0e960a2033eea3
MWPX777 <mwpx777@gmail.com>
2021-04-21 13:04:25 -0500

    updated server.js

--- server.js
+++ server.js
@@ -16,10 +16,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true
+// });
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    
 });
 
 // routes
